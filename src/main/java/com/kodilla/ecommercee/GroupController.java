647d78d956816c31b582bb4aa5d48b710b2b36e8
MajaBrog <majaa.brog@gmail.com>
2020-01-29 20:59:29 +0100

    initial endpoint implementation

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,4 +1,22 @@
 package com.kodilla.ecommercee;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GroupController {
+    public List<String> getGroups() {
+        return new ArrayList<>();
+    }
+
+    public void addGroup(String groupDto) {
+
+    }
+
+    public String getGroup(Long groupId) {
+        return "GroupDto with Id: groupId";
+    }
+
+    public String updateGroup(String groupDto) {
+        return "Updated groupDto";
+    }
 }
