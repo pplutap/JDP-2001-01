7842b804d5066a9f3f013787f8ed032b964ec772
Skorpien <30352529+Skorpien@users.noreply.github.com>
2020-01-30 12:18:15 +0100

    ProductController - initial endpoints implementation

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,27 @@
 package com.kodilla.ecommercee;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProductController {
+
+    public List<String> getProducts() {
+        return new ArrayList<>();
+    }
+
+    public String getProduct(Long productId) {
+        return "getProduct test";
+    }
+
+    public void addProduct(String productDto) {
+
+    }
+
+    public String updateProduct(String productDto) {
+        return "updateProduct test";
+    }
+
+    public void deleteProduct(Long productId) {
+
+    }
 }
