91fc1a6068058c946752af1396c943738ae0cf81
deseff <blazej.kurjata@gmail.com>
2020-02-04 00:27:08 +0100

    UserController - Initial endpoints implementation (JDP200101-5)

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,26 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1/user")
 public class UserController {
+
+    @RequestMapping(method = RequestMethod.POST, value = "createUser")
+    public void createUser(String userDto) {
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "blockUser")
+    public void blockUser(String userDto) {
+
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getToken")
+    public void getToken(Long userId) {
+
+    }
+
 }
