e3f69476c491f53d485087a9d60f577879b24749
Skorpien <30352529+Skorpien@users.noreply.github.com>
2020-02-05 22:20:06 +0100

    Add class ProductDto and fix ProductController

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,19 +1,20 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.ProductDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("v1/product")
+@RequestMapping("v1/project/products")
 public class ProductController {
 
     @RequestMapping(method = RequestMethod.GET, value = "getProducts")
-    public List<String> getProducts() {
-        return new ArrayList<>();
+    public List<ProductDto> getProducts() {
+        return Arrays.asList(new ProductDto(1L, "Something"), new ProductDto(2L, "SomethingElse"));
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getProduct")
@@ -22,12 +23,12 @@ public class ProductController {
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "addProduct")
-    public void addProduct(String productDto) {
+    public void addProduct(ProductDto productDto) {
 
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
-    public String updateProduct(String productDto) {
+    public String updateProduct(ProductDto productDto) {
         return "updateProduct test";
     }
 
