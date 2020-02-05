c30147aaf0f88d6fb990bdd13b6118031af51493
MajaBrog <majaa.brog@gmail.com>
2020-02-05 20:38:23 +0100

    GroupDto added and implemented in GroupController methods

--- src/main/java/com/kodilla/ecommercee/GroupController.java
+++ src/main/java/com/kodilla/ecommercee/GroupController.java
@@ -1,30 +1,34 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.GroupDto;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
-@RequestMapping("v1/project")
+@RequestMapping("v1/project/groups")
 
 public class GroupController {
-    @RequestMapping(method= RequestMethod.GET, value="getGroups")
-    public List<String> getGroups() {
-        return new ArrayList<>();
+    @RequestMapping(method = RequestMethod.GET, value = "getGroups")
+    public List<GroupDto> getGroups() {
+        return Arrays.asList(new GroupDto(1L,"Group1"),new GroupDto(2L,"Group2"));
     }
-    @RequestMapping(method=RequestMethod.POST, value="addGroup")
-    public void addGroup(String groupDto) {
+
+    @RequestMapping(method = RequestMethod.POST, value = "addGroup")
+    public void addGroup(GroupDto groupDto) {
 
     }
-    @RequestMapping(method=RequestMethod.GET, value="getGroup")
-    public String getGroup(Long groupId) {
-        return "GroupDto with Id: groupId";
+
+    @RequestMapping(method = RequestMethod.GET, value = "getGroup")
+    public GroupDto getGroup(Long groupId) {
+        return new GroupDto(1L,"Group1");
     }
-    @RequestMapping(method=RequestMethod.PUT, value="updateGroup")
-    public String updateGroup(String groupDto) {
-        return "Updated groupDto";
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateGroup")
+    public GroupDto updateGroup(GroupDto groupDto) {
+        return new GroupDto(1L,"Updated Group1");
     }
 }
