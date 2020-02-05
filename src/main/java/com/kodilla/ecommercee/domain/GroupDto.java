23debd1e2be0ee6e5783d2d91f7dc5d440521b25
MajaBrog <majaa.brog@gmail.com>
2020-02-05 20:52:40 +0100

    annotations changed

--- src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
+++ src/main/java/com/kodilla/ecommercee/domain/GroupDto.java
@@ -1,10 +1,12 @@
 package com.kodilla.ecommercee.domain;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class GroupDto {
     private Long id;
     private String name;
