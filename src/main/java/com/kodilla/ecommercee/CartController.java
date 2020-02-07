f510b0243fd090add1a57bc66bcedb27380b1dea
Olafbb <olafbodyk@interia.pl>
2020-02-07 02:20:39 +0100

    CartController and CartDto were created

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,4 +1,35 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.CartDto;
+import com.kodilla.ecommercee.domain.ProductDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("v1/project/carts")
 public class CartController {
+    @RequestMapping(method = RequestMethod.POST, value = "createCart")
+    public void createCart() {
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProducts")
+    public List<ProductDto> getProducts() {
+        return Arrays.asList(new ProductDto(1L, "product1"), new ProductDto(2L, "product2"));
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "addProduct")
+    public void addProduct(ProductDto productDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
+    public void deleteProduct(ProductDto productDto) {
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createOrder")
+    public void createOrder(CartDto cartDto) {
+    }
 }
