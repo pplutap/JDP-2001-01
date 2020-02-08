bed2592baa59b4e7456223d93a138982c4c514e0
Skorpien <30352529+Skorpien@users.noreply.github.com>
2020-02-09 00:30:30 +0100

    User entity - first implementation

---
+++ src/main/java/com/kodilla/ecommercee/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.kodilla.ecommercee.dao;
+
+import com.kodilla.ecommercee.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Repository
+public interface UserDao extends CrudRepository<User, Long> {
+}
