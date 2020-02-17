c20b773757115d65b3d85481e92180b62b43c61e
sebastiankalinkowski <Polska500+>
2020-02-17 17:36:50 +0100

    JDP200101-4 changes fixes after second review

--- src/main/java/com/kodilla/ecommercee/controller/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/controller/OrderController.java
@@ -1,11 +1,7 @@
 package com.kodilla.ecommercee.controller;
 
 
-import com.kodilla.ecommercee.domain.Order;
 import com.kodilla.ecommercee.domain.OrderDto;
-import com.kodilla.ecommercee.service.DbService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,15 +12,10 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping(value = "/v1/project/order", consumes = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/v1/project/order")
 
 
 public class OrderController {
-
-
-    @Autowired
-    private DbService service;
-
 
     @RequestMapping(method = RequestMethod.GET, value = "getOrders")
     public List<OrderDto> getOrders() {
@@ -32,17 +23,14 @@ public class OrderController {
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getOrder")
-    public Order getOrder(@RequestParam Long orderId) throws OrderNotFoundException{
-        return service.getOrder(1L).orElseThrow(OrderNotFoundException::new);
+    public OrderDto getOrder( @RequestParam Long orderId) throws OrderNotFoundException{
+        return new OrderDto(1L,"Order_One");
     }
-
 
     @RequestMapping(method = RequestMethod.POST, value = "addOrder")
     public OrderDto addOrder(OrderDto orderDto) {
-        return new OrderDto(4L, "Order_four");
+        return new OrderDto(4L, "Order Added");
     }
-
-
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateOrder")
     public OrderDto updateOrder(Long orderId) {
@@ -51,8 +39,6 @@ public class OrderController {
 
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteOrder")
     public void deleteOrder(@RequestParam Long orderId) {
-        service.deleteOrder(orderId);
-
 
     }
 }
