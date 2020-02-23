cb039ef704c0fc983cdf5eec5a99939124a3b192
Mateusz Ryndak <kontakt@mryndak.pl>
2020-02-23 19:50:47 +0100

    JDP200101-11 Encja Order - revert comments in EcommerceeApplicationTests

--- src/test/java/com/kodilla/ecommercee/EcommerceeApplicationTests.java
+++ src/test/java/com/kodilla/ecommercee/EcommerceeApplicationTests.java
@@ -1,5 +1,5 @@
 package com.kodilla.ecommercee;
-/*
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,4 +15,3 @@ public class EcommerceeApplicationTests {
 
 }
 
-*/
